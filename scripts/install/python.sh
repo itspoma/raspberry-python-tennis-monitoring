3ee754b22b049f288dcab12138a2c7477643218f
itspoma <itspoma@gmail.com>
2014-09-14 02:10:49 +0300

    added pip

--- scripts/install/python.sh
+++ scripts/install/python.sh
@@ -5,6 +5,7 @@ if ! which python >/dev/null 2>/dev/null; then
     echo "INSTALL: mc"
 
     sudo yum install -y python
+    sudo yum -y install python-pip
 else
     echo "VERSION: python: $(python -V)"
 fi
